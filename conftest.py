04a2cfda3cf59dd6df8c3e94f7a95b1700efd0c6
peresypkin.maQA <peresypkin.ma@sibvaleo.com>
2023-03-19 18:56:41 +0700

    test complete

--- conftest.py
+++ conftest.py
@@ -2,12 +2,7 @@ import os
 import pytest
 
 
-
 @pytest.fixture
 def clear_file_before_test():
     if os.path.exists(os.path.abspath(os.path.join('resources', 'myzip.zip'))):
         os.remove(os.path.abspath(os.path.join('resources', 'myzip.zip')))
-
-
-
-
